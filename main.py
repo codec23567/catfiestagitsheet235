2f7cda91174e72b66273113aee0d141079569424
codec23567 <parkjisang0808@gmail.com>
2026-07-21 20:26:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,21 +22,42 @@ spreadsheet = gc.open_by_key(
 
 worksheet = spreadsheet.sheet1
 
+
+
 start_row = 5
 
-urls = worksheet.col_values(3)   # C열
+urls = worksheet.col_values(3)      # C열
+dates = worksheet.col_values(5)     # E열
+authors = worksheet.col_values(6)   # F열
 
-for row, url in enumerate(urls[start_row - 1:], start=start_row):
+requests = []
+target_rows = []
 
-    if url.strip() == "":
+for row in range(start_row, len(urls) + 1):
+
+    url = urls[row - 1].strip()
+
+    date = dates[row - 1] if row - 1 < len(dates) else ""
+    author = authors[row - 1] if row - 1 < len(authors) else ""
+
+    if not url:
         continue
 
+    if "dcinside" not in url:
+        continue
+
+    is_clear = (date == "") or (author == "")
+    is_retry = (date == "retry")
+
+    if is_clear or is_retry:
+        requests.append(url)
+        target_rows.append(row)
+
+print("크롤링 대상 개수 :", len(requests))
+
+for row, url in zip(target_rows, requests):
     print(row, url)
-    result = extract_nickdate(url)
 
-    print(result)
-
-    break
 
 
 worksheet.update_acell("A1", "GitHub Actions 성공!")
