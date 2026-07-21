b88a5a4bd7c8997c1ee1104b74bd16ef35023c38
codec23567 <parkjisang0808@gmail.com>
2026-07-21 19:58:06 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+
+credentials = Credentials.from_service_account_info(
+    json.loads(os.environ["GOOGLE_CREDENTIALS"]),
+    scopes=SCOPES
+)
+
+gc = gspread.authorize(credentials)
+
+spreadsheet = gc.open_by_key(
+    "1_4rB1Tk248VBqkMT6MhywlV-_m0hitwWv2MkiQ9hzAU"
+)
+
+worksheet = spreadsheet.sheet1
+
+print("A1 =", worksheet.acell("A1").value)
