9886bc572fa9ab387f920bc6413f9faf8a82326d
codec23567 <parkjisang0808@gmail.com>
2026-07-22 16:26:22 +0900

    Update login_modify_html.py

--- modify_html_ver/login_modify_html.py
+++ modify_html_ver/login_modify_html.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import time
-
+import traceback
 
 LOGIN_URL = (
     "https://sign.dcinside.com/login"
